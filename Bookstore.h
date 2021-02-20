41ee51f88c7ab1bddf993e725d4dff13e09e0519
Monta <74357572+Montayang@users.noreply.github.com>
2021-02-20 16:51:25 +0800

    Create Bookstore.h

---
+++ Bookstore.h
@@ -0,0 +1,33 @@
+#ifndef BOOKSTORE_BOOKSDTORE_H
+#define BOOKSTORE_BOOKSDTORE_H
+
+#include "blocklist.h"
+#include "user.h"
+
+class book {
+public:
+    char name[60] = "";
+    char ISBN[20] = "";
+    char author[60] = "";
+    char keyword[60] = "";
+    double price = 0;
+    int quantity = 0;
+
+    book()=default;
+
+    book(const string &ISBN1, const string &name1, const string &author1, const string &keyword1,
+         const double &price1,int quantity1);
+};
+
+class transaction_record {
+public:
+    double money=0;
+    book operate_book;
+    int book_quantity=0;
+    user operate_user;
+    transaction_record()=default;
+    transaction_record(double money1, const book& book1, int book_quantity1, const user& user1);
+};
+
+book the_book(const string& ISBN1);
+#endif //BOOKSTORE_BOOKSDTORE_H
